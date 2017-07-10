e5291968fec47bb7049ed6474f4e613de2c41c6f
blghns <bilgehan.saglik@gmail.com>
2017-07-10 11:26:17 -0400

    Fixed redrawing order bug

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
 var circles = [];
-var strokes = [];
 var fillSize = 80;
 
 function setup() {
@@ -10,60 +9,24 @@ function setup() {
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   background(0);
-  circles.forEach(e => {
-    var r = map(noise(e[0]), 0, 1, 0, 256);
-    var g = map(noise(e[1]), 0, 1, 0, 256);
-    var b = map(noise(e[2]), 0, 1, 0, 256);
-    var a = map(noise(e[2]), 0, 1, 0, 256);
-    fill(r, g, b, a);
-    noStroke();
-    ellipse(...e);
-  });
-
-  strokes.forEach(e => {
-    var r = map(noise(e[0]), 0, 1, 0, 256);
-    var g = map(noise(e[1]), 0, 1, 0, 256);
-    var b = map(noise(e[2]), 0, 1, 0, 256);
-    var a = map(noise(e[2]), 0, 1, 0, 256);
-    noFill();
-    stroke(r, g, b, a);
-    ellipse(...e);
-  });
+  circles.forEach(c => c.display());
 }
 
 function draw() {
   if (touches.length == 1) {
-    var e = [touches[0].x, touches[0].y, fillSize]
-    circles.push(e);
+    var c = new circle(touches[0].x, touches[0].y, fillSize);
+    circles.push(c);
   } else if (touches.length == 2) {
     var midX = (touches[0].x + touches[1].x) / 2;
     var midY = (touches[0].y + touches[1].y) / 2;
     var d = dist(touches[0].x, touches[0].y, touches[1].x, touches[1].y);
-    var e = [midX, midY, d];
-    strokes.push(e);
+    var c = new circle(midX, midY, d, "stroke");
+    circles.push(c);
     fillSize = d;
   }
-
-  if (e) {
-    var r = map(noise(e[0]), 0, 1, 0, 256);
-    var g = map(noise(e[1]), 0, 1, 0, 256);
-    var b = map(noise(e[2]), 0, 1, 0, 256);
-    var a = map(noise(e[2]), 0, 1, 0, 256);
-    if (touches.length == 1) {
-      fill(r, g, b, a);
-      noStroke();
-      ellipse(...e);
-    } else if (touches.length == 2) {
-      noFill();
-      stroke(r, g, b, a);
-      ellipse(...e);
-    }
+  if (c) {
+    c.display();
   }
-
-  
-
-
-
 }
 
 // this prevents dragging screen around
@@ -75,5 +38,27 @@ function touchEnded() {
   if (touches.length == 2) {
     var fs = fullscreen();
     fullscreen(!fs);
+  }
+}
+
+
+function circle(x, y, d, fillOrStroke = "fill") {
+  this.x = x;
+  this.y = y;
+  this.d = d;
+  this.fillOrStroke = fillOrStroke;
+  this.display = function() {
+    var r = map(noise(this.x / 10), 0, 1, 0, 256);
+    var g = map(noise(this.y / 10), 0, 1, 0, 256);
+    var b = map(noise(this.d / 10), 0, 1, 0, 256);
+    var a = map(noise(r + g + b), 0, 1, 0, 256);
+    if (this.fillOrStroke === "fill") {
+      fill(r, g, b, a);
+      noStroke();
+    } else if (this.fillOrStroke === "stroke") {
+      noFill();
+      stroke(r, g, b, a);
+    }
+    ellipse(this.x, this.y, this.d);
   }
 }
